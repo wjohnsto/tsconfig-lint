c4e56da2ba03bea8ef8cca5cddc68cc359cb1deb
Will Johnston <will@getplatypi.com>
2015-12-14 13:54:54 -0600

    fixing to work with new tsconfig-glob

--- index.ts
+++ index.ts
@@ -10,47 +10,47 @@ var Linter = require('tslint');
 var tsconfig = require('tsconfig-glob');
 
 function log(color?: Array<number>, lighten: number = 0): (message: string) => void {
-	var strColor: Array<string>;
+    var strColor: Array<string>;
 
-	if(Array.isArray(color)) {
-		strColor = ['\033[' + (color[0] + lighten) + 'm', '\033[' + color[1] + 'm'];
-	} else {
-		strColor = ['', ''];
-	}
+    if (Array.isArray(color)) {
+        strColor = ['\033[' + (color[0] + lighten) + 'm', '\033[' + color[1] + 'm'];
+    } else {
+        strColor = ['', ''];
+    }
 
-	return function(message: string): void {
-		console.log(strColor[0] + message + strColor[1]);
-	};
+    return function(message: string): void {
+        console.log(strColor[0] + message + strColor[1]);
+    };
 }
 
 var colors = (<any>util.inspect).colors,
-	red = log(colors.red, 60),
-	green = log(colors.green),
-	cyan = log(colors.cyan),
-	normal = log();
+    red = log(colors.red, 60),
+    green = log(colors.green),
+    cyan = log(colors.cyan),
+    normal = log();
 
 function unique(arr: Array<string>): Array<string> {
-	var keys: { [key: string]: boolean; } = {},
-		out: Array<string> = [];
+    var keys: { [key: string]: boolean; } = {},
+        out: Array<string> = [];
 
-	for(var i = 0, l = arr.length; i < l; ++i) {
-		if(keys.hasOwnProperty(arr[i])) {
-			continue;
-		}
+    for (var i = 0, l = arr.length; i < l; ++i) {
+        if (keys.hasOwnProperty(arr[i])) {
+            continue;
+        }
 
-		out.push(arr[i]);
-		keys[arr[i]] = true;
-	}
+        out.push(arr[i]);
+        keys[arr[i]] = true;
+    }
 
-	return out;
+    return out;
 }
 
 function findRules(config: { rules?: any; lintOptions?: any; }): { rules?: any; } {
-	if(config.hasOwnProperty('rules')) {
-		return config;
-	}
+    if (config.hasOwnProperty('rules')) {
+        return config;
+    }
 
-	return config.lintOptions;
+    return config.lintOptions;
 }
 
 var es6 = false;
@@ -59,7 +59,7 @@ function lintFile(file: string, config: { configuration?: { rules?: any; } }): A
     try {
         return [(new Linter(file, fs.readFileSync(file, 'utf8'), config)).lint(), file];
     } catch (e) {
-        if(!es6 && e.message.indexOf('Cannot read property \'text\' of undefined') > -1) {
+        if (!es6 && e.message.indexOf('Cannot read property \'text\' of undefined') > -1) {
             es6 = true;
             delete config.configuration.rules.whitespace;
             delete config.configuration.rules['no-use-before-declare'];
@@ -78,103 +78,114 @@ We will remove the following rules allow linting files temporarily to lint these
 }
 
 function lintFiles(files: Array<string>, config: { formatter?: string; configuration?: { rules?: any; } }): number {
-	var failed = 0;
+    var failed = 0;
 
-	cyan('Linting ' + files.length + ' file' + (files.length === 1 ? '' : 's'));
+    cyan('Linting ' + files.length + ' file' + (files.length === 1 ? '' : 's'));
 
-	files.map((file) => {
-			return lintFile(file, config);
-		})
-		.sort((resultA, resultB) => {
-			var a: { failureCount: number; } = resultA[0],
-				b: { failureCount: number; } = resultB[0];
+    files.map((file) => {
+        return lintFile(file, config);
+    })
+        .sort((resultA, resultB) => {
+            var a: { failureCount: number; } = resultA[0],
+                b: { failureCount: number; } = resultB[0];
 
-			if(a.failureCount > b.failureCount) {
-				return 1;
-			}
+            if (a.failureCount > b.failureCount) {
+                return 1;
+            }
 
-			if(b.failureCount > a.failureCount) {
-				return -1;
-			}
+            if (b.failureCount > a.failureCount) {
+                return -1;
+            }
 
-			return 0;
-		})
-		.map((results) => {
-			var result = results[0];
+            return 0;
+        })
+        .map((results) => {
+            var result = results[0];
 
-			if(result.failureCount <= 0) {
-				return '';
-			}
+            if (result.failureCount <= 0) {
+                return '';
+            }
 
-			failed += result.failureCount;
+            failed += result.failureCount;
 
-			return result.output
-				.split(/\r\n|\n/)
-				.map((line: string) => {
-					if(line === '') {
-						return;
-					}
+            return result.output
+                .split(/\r\n|\n/)
+                .map((line: string) => {
+                    if (line === '') {
+                        return;
+                    }
 
-					return line;
-				}).join(EOL);
-		}).forEach((value) => {
-			if(!value) {
-				return;
-			}
+                    return line;
+                }).join(EOL);
+        }).forEach((value) => {
+            if (!value) {
+                return;
+            }
 
-			normal(value);
-		});
+            normal(value);
+        });
 
-	return failed;
+    return failed;
 }
 
-export = function(options: IOptions): number {
-	var root = options.cwd || process.cwd(),
-		configDir = path.resolve(root, options.configPath || '.'),
-		filePath: string;
+export = function(options: IOptions, done: (err?: any, success?: number) => void): void {
+    var root = options.cwd || process.cwd(),
+        configDir = path.resolve(root, options.configPath || '.'),
+        filePath: string;
 
-	if(configDir.indexOf('.json') === -1) {
-		filePath = path.resolve(configDir, 'tsconfig.json');
-	} else {
-		filePath = configDir;
-	}
+    if (configDir.indexOf('.json') === -1) {
+        filePath = path.resolve(configDir, 'tsconfig.json');
+    } else {
+        filePath = configDir;
+    }
 
-	var configFile: { filesGlob: Array<string>; files: Array<string>; } = require(filePath),
-		useGlob = options.useGlob;
+    var configFile: { filesGlob: Array<string>; files: Array<string>; } = require(filePath),
+        useGlob = options.useGlob;
 
-	if(useGlob) {
-		configFile = tsconfig({
-			cwd: root,
-			configPath: options.configPath,
-			indent: options.tsconfigOptions && options.tsconfigOptions.indent
-		});
-	}
+    if (useGlob) {
+        configFile = tsconfig({
+            cwd: root,
+            configPath: options.configPath,
+            indent: options.tsconfigOptions && options.tsconfigOptions.indent
+        }, (err: any) => {
+            if (!!err) {
+                done(err);
+                return;
+            }
 
-	var	files = unique(configFile.files || []),
-		configuration = extend(true, undefined, defaultRules, findRules(configFile));
+            lint();
+        });
+    } else {
+        lint();
+    }
 
-	var failed = lintFiles(files, {
-		formatter: 'prose',
-		configuration: configuration
-	});
+    function lint(): void {
+        var files = unique(configFile.files || []),
+            configuration = extend(true, undefined, defaultRules, findRules(configFile));
 
-	var message = 'Done with ' + failed + ' failures.';
+        var failed = lintFiles(files, {
+            formatter: 'prose',
+            configuration: configuration
+        });
 
-	if(failed > 0) {
-		red(message);
-	} else {
-		green(message);
-	}
+        var message = 'Done with ' + failed + ' failures.';
 
-	return failed;
+        if (failed > 0) {
+            red(message);
+        } else {
+            green(message);
+        }
+
+        done(undefined, failed);
+    }
 };
 
 interface IOptions {
-	args?: Array<string>;
-	configPath?: string;
-	cwd?: string;
-	useGlob?: boolean;
-	tsconfigOptions: {
-		indent?: number;
-	};
+    args?: Array<string>;
+    configPath?: string;
+    cwd?: string;
+    useGlob?: boolean;
+    tsconfigOptions: {
+        indent?: number;
+    };
 }
